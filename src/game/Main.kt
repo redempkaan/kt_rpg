573483cb1b0af9561596dd74f552a50b52ec2384
Kaan Menevşe <kaanmen@Kaans-MacBook-Pro.local>
2025-08-26 01:43:59 +0300

    first init

--- src/game/Main.kt
+++ src/game/Main.kt
@@ -1,12 +1,10 @@
 package src.game
 
 import src.model.warrior.Warrior
-//import src.model.Arena
+
 
 
 fun main(){
-
-    //val arena = Arena()
 
     val warrior1 = Warrior("Bert")
     val warrior2 = Warrior("Makan")
@@ -14,9 +12,6 @@ fun main(){
     warrior1.listSkills()
 
     warrior1.useSkillById(1, warrior2)
-
-
-    //arena.startDuel(warrior1, warrior2)
 
 
 }
