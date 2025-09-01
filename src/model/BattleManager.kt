c695d67b60a544411fb792b8563c00a8b0891b0e
Kaan Menevşe <kaanmen@Kaans-MacBook-Pro.local>
2025-09-01 16:54:03 +0300

    Exception handling

--- src/model/BattleManager.kt
+++ src/model/BattleManager.kt
@@ -5,26 +5,57 @@ import src.model.characterType.Combatant
 class BattleManager() {
 
     fun pickCombatants(combatantList: List<Combatant>): Pair<Combatant, Combatant> {
-
-        val indexC1: Int
-        val indexC2: Int
+        var indexC1: Int
+        var indexC2: Int
+        var input: Int
         //Listing combatant with their descriptions for user to select combatants
         combatantList.forEachIndexed {index, combatant ->
             println("\n${index + 1}) ${combatant.name}")
             combatant.descriptChar()
         }
 
-        println("\nEnter the id of the character to play as")
-        indexC1 = readln().toInt() - 1
+        indexC1 = -1
 
+        println("\nEnter the id of the character to play as (1 - ${combatantList.size})")
+        while(indexC1 == -1) {
+            try { // Checking input exception
+                input = readln().toInt()
+                combatantList[input - 1]
+                indexC1 = input - 1
+            }
+            catch (e: Exception) {
+                when(e) {
+                    is IndexOutOfBoundsException -> println("Input value must be between 1 and ${combatantList.size}")
+                    is NumberFormatException -> println("The input value must be a number")
+                }
+            }
+
+        }
 
 
         combatantList.forEachIndexed {index, combatant ->
             println("\n${index + 1}) ${combatant.name}")
             combatant.descriptChar()
         }
+
+        indexC2 = -1
         println("\nEnter the id of the opponent to fight")
-        indexC2 = readln().toInt() - 1
+
+        while(indexC2 == -1) {
+            try { // Checking input exception
+                input = readln().toInt()
+                combatantList[input - 1]
+                indexC2 = input - 1
+            }
+            catch (e: Exception) {
+                when(e) {
+                    is IndexOutOfBoundsException -> println("Input value must be between 1 and ${combatantList.size}")
+                    is NumberFormatException -> println("The input value must be a number")
+                }
+            }
+
+        }
+
         // Returns picked combatants
         return Pair(combatantList[indexC1], combatantList[indexC2])
 
@@ -48,14 +79,27 @@ class BattleManager() {
                 println("\nThe turn is yours!")
                 combatant1.listSkills()
                 println("Enter the skill's id that you want to use")
-                skillId = readln().toInt()
-                dealedDamage = combatant1.useSkillById(skillId, combatant2)
+
+                dealedDamage = -1
+                while(dealedDamage == -1){
+                    try { //Checking input exception
+                        skillId = readln().toInt()
+                        dealedDamage = combatant1.useSkillById(skillId, combatant2)
+                    }
+                    catch (e: Exception) {
+                        when (e) {
+                            is NullPointerException -> println("Invalid skill ID, please look up for your skill list")
+                            is NumberFormatException -> println("The skill ID must be a number")
+                        }
+                    }
+                }
                 tempHp2 -= dealedDamage
             }
-            else{
+            else {
                 dealedDamage = combatant2.useSkillById(1, combatant1)
                 tempHp1 -= dealedDamage
             }
+
             println("\nTurn ${roundCounter}:  ${combatant1.name} : ${tempHp1},  ${combatant2.name} : ${tempHp2}\n")
             roundCounter++
         }
